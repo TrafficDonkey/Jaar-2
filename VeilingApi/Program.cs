0fea9b958c1c8c7e3dcae880ec5569ffee00a874
khalid3385 <khalid20hossam07@gmail.com>
2026-01-18 14:14:30 +0100

    Add new origin to CORS policy

--- VeilingApi/Program.cs
+++ VeilingApi/Program.cs
@@ -67,17 +67,18 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("web", policy =>
     {
-       policy.WithOrigins(
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-    "http://localhost:5174",
-    "http://127.0.0.1:5174",
-    "https://jaar-2frontendsem3.vercel.app",
-    "https://jaar-2frontendsem3-5adthyzyh-khalid3385s-projects.vercel.app")
-.AllowAnyHeader()
-.AllowAnyMethod()
-.AllowCredentials();
-
+        policy.WithOrigins(
+            "http://localhost:5173",
+            "http://127.0.0.1:5173",
+            "http://localhost:5174",
+            "http://127.0.0.1:5174",
+            "https://jaar-2frontendsem3.vercel.app",
+            "https://jaar-2frontendsem3-5adthyzyh-khalid3385s-projects.vercel.app",
+            "https://fefaafa.vercel.app"  
+        )
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .AllowCredentials();
     });
 });
 
