42ce56c540480b422bc074b74a0f8629de0ba71b
khalid3385 <khalid20hossam07@gmail.com>
2026-01-18 14:16:37 +0100

    Fix CORS URL for frontend application

--- VeilingApi/Program.cs
+++ VeilingApi/Program.cs
@@ -74,7 +74,7 @@ builder.Services.AddCors(options =>
             "http://127.0.0.1:5174",
             "https://jaar-2frontendsem3.vercel.app",
             "https://jaar-2frontendsem3-5adthyzyh-khalid3385s-projects.vercel.app",
-            "https://fefaafa.vercel.app"  
+            "https://fefaafaa.vercel.app"  
         )
         .AllowAnyHeader()
         .AllowAnyMethod()
