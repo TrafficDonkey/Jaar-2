011ffc77ea7e954bac774f54c907b45fc5991595
khalid33859i <khalid20hossam07@gmail.com>
2026-01-19 11:17:27 +0100

    Fix HealthController GetDbConnection build

--- VeilingApi/Controllers/HealthController.cs
+++ VeilingApi/Controllers/HealthController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using VeilingApi.Data;
 
 namespace VeilingApi.Controllers;
